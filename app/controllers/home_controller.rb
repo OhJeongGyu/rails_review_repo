284231a2442f13042f4c6e05c3040d3595148199
OhJeong Gyu <ohjeonggyu@OhJeongui-iMac.local>
2016-07-11 00:01:25 +0900

    crud 중 cr

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,19 @@
 class HomeController < ApplicationController
   def index
-
+    @all_posts = Post.all
   end
 
   def write
-    @title = params[:title]
-    @content = params[:content]
+    p = Post.new
+    p.title = params[:title]
+    p.content = params[:content]
+    p.save
+
+    redirect_to "/index"
   end
+
+  def list
+  end
+
+
 end
